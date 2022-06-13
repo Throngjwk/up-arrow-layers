5c66e5e8f6d8b71d8fc520e2b82546821f927691
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-06-13 19:13:20 -0300

    Update changelog-tab.js

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,6 +1,10 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
+    <template v-slot:title>v1.0.3</template>
+    <template v-slot:text>update guide tab
+    </template>
+    <guide-item>
     <template v-slot:title>v1.0.2</template>
     <template v-slot:text>wow this 2 new layers and 1 nnew achievement!
     </template>
